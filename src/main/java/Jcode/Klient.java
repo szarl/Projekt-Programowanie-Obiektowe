e04dd5e6fd3f67af83b1d6c6c638aea81959ecde
Mimbla <259064@student.pwr.edu.pl>
2021-05-30 18:37:59 +0200

    klient

---
+++ src/main/java/Jcode/Klient.java
@@ -0,0 +1,25 @@
+package Jcode;
+
+public class Klient {
+    private int numer_telefonu;
+    private boolean plec;
+    private float wzrost;
+    private float waga;
+    private float waga_docelowa;
+    private int zapotrzebowanie;
+    private int wiek;
+    private String adres_email;
+    private String data_zapisu;
+    private String data_koncowa;
+
+    private float ObliczBmi(float wzrost, float waga, int wiek, boolean plec) {
+
+        return 0;
+    }
+
+    protected int ObliczZapotrzebowanie(float, float, int, boolean, float) {
+
+        return 0;
+    }
+
+}
---
+++ src/main/java/Jcode/Trener.java
@@ -0,0 +1,4 @@
+package Jcode;
+
+public class Trener {
+}
