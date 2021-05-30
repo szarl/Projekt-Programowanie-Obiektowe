f43b2d1f2c155b0f8a2b88dc2ca48c6655eab83d
Mimbla <259064@student.pwr.edu.pl>
2021-05-30 18:56:40 +0200

    klient

--- src/main/java/Jcode/Klient.java
+++ src/main/java/Jcode/Klient.java
@@ -1,16 +1,16 @@
 package Jcode;
 
 public class Klient {
-    private int numer_telefonu;
+    private int numerTelefonu;
     private boolean plec;
     private float wzrost;
     private float waga;
-    private float waga_docelowa;
+    private float wagaDocelowa;
     private int zapotrzebowanie;
     private int wiek;
-    private String adres_email;
-    private String data_zapisu;
-    private String data_koncowa;
+    private String adresEmail;
+    private String dataZapisu;
+    private String dataKoncowa;
 
     private float ObliczBmi(float wzrost, float waga, int wiek, boolean plec) {
 
