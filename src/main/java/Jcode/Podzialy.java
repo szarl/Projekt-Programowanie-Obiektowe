ed36aec6bd45f439b71d051382fe2a4c7295f541
Mimbla <259064@student.pwr.edu.pl>
2021-06-06 13:03:25 +0200

    podzialy

---
+++ src/main/java/Jcode/Podzialy.java
@@ -0,0 +1,10 @@
+package Jcode;
+
+public abstract class Podzialy {
+    protected String nazwa;
+    protected int czas;
+    protected String pora;
+    protected String opis;
+
+    protected abstract void sprawdz();
+}
